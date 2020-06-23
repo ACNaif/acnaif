29adeda966c8654566a4edd4a94e2fe80d0e2608
Naif Alfirm <Nfahad40@gmail.com>
2020-06-23 06:24:03 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import authors from "./data"
+import MyFirstC from "./MyFirstC";
+function App() {
+  return (
+<div>
+  <MyFirstC />
+  </div>
+  );
+}
+
+export default App;
